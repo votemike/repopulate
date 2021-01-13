19d155cc129879a8cf21a3d30213f63a6d2adfc5
Michael Gwynne <michael@votemike.co.uk>
2021-01-13 22:37:01 +0000

    Michael

--- src/components/Person.js
+++ src/components/Person.js
@@ -5,7 +5,6 @@ const Person = ({ name, sex, mate, mother, father, setPeople, people, selectMate
   <li>
     <span style={{color: sex === 'male' ? 'blue' : 'pink'}}>{name}</span>
     {sex === 'female' && <>{' '} - <Pairer people={people} femaleName={name} mate={mate} selectMate={selectMate} setPeople={setPeople}/></>}
-    {/*{mother && father && <ul><li>{mother}</li><li>{father}</li></ul>}*/}
   </li>
 );
 
